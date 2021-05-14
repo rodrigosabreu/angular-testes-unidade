5b927116fe5cef51c1fafcfe53ae2f01bb7da3e2
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2021-05-14 12:28:17 -0300

    karma e protractor

---
+++ src/app/demos/todo-list/todo.store.ts
@@ -0,0 +1,33 @@
+import { Task } from './task';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+
+export interface State {
+    todolist: Task[]
+}
+
+const state: State = {
+    todolist: []
+};
+
+@Injectable()
+export class Store {
+    private subject = new BehaviorSubject<State>(state);
+    private store = this.subject.asObservable();
+
+    get value() {
+        return this.subject.value;
+    }
+
+    public getTodoList(): Observable<Task[]> {
+        return this.store
+            .pipe(map(store => store.todolist));
+    }
+
+    set(name: string, state: any) {
+        this.subject.next({
+            ...this.value, [name]: state
+        });
+    }
+}
